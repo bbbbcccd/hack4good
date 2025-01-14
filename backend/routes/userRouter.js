eac6d045c54cd95f0ec36164f5a558ac023da1e7
Zac <zacheryngky@gmail.com>
2025-01-14 23:29:58 +0800

    Remove duplicate file

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -19,11 +19,4 @@ userRouter.get("/", UserController.getUserDetails);
 
 // View task completion history
 
-indexRouter.get("/", async (req, res) => {
-  await pool.query(
-    "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR(255), age INT)"
-  );
-  res.send("Success!");
-});
-
 export default userRouter;
