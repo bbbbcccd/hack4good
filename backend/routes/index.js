28e43205ae13ac4772f584d40c70dd4d89c5f61f
wesley <wesleywong08@gmail.com>
2025-01-14 15:35:00 +0800

    Update docker config

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,27 +1,27 @@
-import express from "express";
+import express from 'express';
 
-import pool from "../database/db.js";
+import pool from '../database/db.js';
 
 const indexRouter = express.Router();
 
-indexRouter.get("/", async (req, res) => {
+indexRouter.get('/', async (req, res) => {
   await pool.query(
-    "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR(255), age INT)"
+    'CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR(255), age INT)',
   );
-  res.send("Success!");
+  res.send('Success!');
 });
 
-indexRouter.post("/users", async (req, res) => {
-  const { name, age } = req.body;
+indexRouter.post('/users', async (req, res) => {
+  const { username, password, vouchers, phone_number } = req.body;
   const response = await pool.query(
-    "INSERT INTO users (name, age) VALUES ($1, $2)",
-    [name, age]
+    'INSERT INTO users (username, password, vouchers, phone_number) VALUES ($1, $2, $3, $4)',
+    [username, password, vouchers, phone_number],
   );
-  res.send("success");
+  res.send('success');
 });
 
-indexRouter.get("/users", async (req, res) => {
-  const response = await pool.query("SELECT * FROM users");
+indexRouter.get('/users', async (req, res) => {
+  const response = await pool.query('SELECT * FROM users');
   res.send(response.rows);
 });
 
