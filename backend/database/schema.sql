f7d45f13aad93be4691fa1ccabbf076cf611d442
Zac <zacheryngky@gmail.com>
2025-01-15 16:43:49 +0800

    Drop all tables and types before creating

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,3 +1,13 @@
+-- Drop all existing tables and types
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS admins CASCADE;
+DROP TABLE IF EXISTS items CASCADE;
+DROP TABLE IF EXISTS transactions CASCADE;
+DROP TABLE IF EXISTS tasks CASCADE;
+DROP TABLE IF EXISTS task_completions CASCADE;
+DROP TYPE IF EXISTS TRANSACTION_STATUS CASCADE;
+DROP TYPE IF EXISTS TASK_STATUS CASCADE;
+
 CREATE TABLE IF NOT EXISTS
   users (
     username VARCHAR(256) PRIMARY KEY,
@@ -19,7 +29,7 @@ items (
   quantity INTEGER NOT NULL CHECK (quantity >= 0)
 );
 
-CREATE TYPE IF NOT EXISTS TRANSACTION_STATUS AS ENUM ('paid', 'received', 'pre-ordered');
+CREATE TYPE TRANSACTION_STATUS AS ENUM ('paid', 'received', 'pre-ordered');
 
 CREATE TABLE IF NOT EXISTS
 transactions (
@@ -37,7 +47,7 @@ tasks (
   reward NUMERIC NOT NULL
 );
 
-CREATE TYPE TASK_STATUS IF NOT EXISTS AS ENUM ('requested', 'approved', 'rejected');
+CREATE TYPE TASK_STATUS AS ENUM ('requested', 'approved', 'rejected');
 
 CREATE TABLE IF NOT EXISTS
 task_completions (
