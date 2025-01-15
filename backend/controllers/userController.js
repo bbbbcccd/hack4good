b2a9703f0c583e7f1f09f076eac7ea1f09353376
Zac <zacheryngky@gmail.com>
2025-01-15 19:26:14 +0800

    Add check for missing username or password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,10 @@ import pool from "../database/db.js";
 export const getUserDetails = async (req, res) => {
   const { username, password } = req.body;
 
+  if (!username || !password) {
+    return res.status(400).json({ msg: "Missing required fields" });
+  }
+
   await pool
     .query("SELECT * FROM users WHERE username = $1 AND password = $2", [
       username,
@@ -10,7 +14,7 @@ export const getUserDetails = async (req, res) => {
     ])
     .then((data) => {
       if (data.rows.length != 1) {
-        res.status(400).json({ msg: "Invalid user" });
+        return res.status(400).json({ msg: "Invalid user" });
       }
 
       res.send(data.rows[0]);
