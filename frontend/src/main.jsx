57ed41587ff1400db7fd288ba5db79c5d9ef44d1
Zac <zacheryngky@gmail.com>
2025-01-16 21:57:49 +0800

    Create transaction context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,15 +5,18 @@ import "./index.css";
 import App from "./App.jsx";
 import { AuthContextProvider } from "./contexts/AuthContext.jsx";
 import { MinimartContextProvider } from "./context/MinimartContext.jsx";
+import { TransactionContextProvider } from "./context/users/TransactionContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthContextProvider>
-      <MinimartContextProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </MinimartContextProvider>
+      <TransactionContextProvider>
+        <MinimartContextProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </MinimartContextProvider>
+      </TransactionContextProvider>
     </AuthContextProvider>
   </StrictMode>
 );
