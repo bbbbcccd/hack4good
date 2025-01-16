0ec4dc81dbc7129613f6e0d7dca32d6343c56005
Zac <zacheryngky@gmail.com>
2025-01-16 13:46:00 +0800

    Change env variable name

--- frontend/src/util/api.js
+++ frontend/src/util/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.BACKEND_BASE_URL;
+const BASE_URL = import.meta.env.VITE_SERVER_URL;
 
 const axiosPrivate = axios.create({
     baseURL: BASE_URL,
