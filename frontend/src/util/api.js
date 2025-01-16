cfbf6262620f9cc5f891dda39282dd21ec3e5a87
wesley <wesleywong08@gmail.com>
2025-01-16 19:26:09 +0800

    Add private axios hook

--- frontend/src/util/api.js
+++ frontend/src/util/api.js
@@ -1,11 +1,15 @@
-import axios from "axios";
+import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_SERVER_URL;
 
 const axiosPrivate = axios.create({
-    baseURL: BASE_URL,
-    headers: { 'Content-Type': 'application/json' },
-    withCredentials: true,
+  baseURL: BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,
 });
 
-export { BASE_URL, axiosPrivate };
+const axiosPublic = axios.create({
+  baseURL: BASE_URL,
+});
+
+export { BASE_URL, axiosPrivate, axiosPublic };
