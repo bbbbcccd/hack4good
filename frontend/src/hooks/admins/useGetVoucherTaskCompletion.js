46e2d73f94d5e519381d8497352ba2d0cfce8f89
Zac <zacheryngky@gmail.com>
2025-01-17 23:05:30 +0800

    Create hook for admins to view voucher task completions

---
+++ frontend/src/hooks/admins/useGetVoucherTaskCompletion.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+
+import { useAxiosPrivate } from '../auth/useAxiosPrivate.jsx';
+import { useVoucherTaskCompletionContext } from '../commons/useVoucherTaskCompletionContext.js';
+
+const useGetVoucherTaskCompletion = () => {
+    const axiosPrivate = useAxiosPrivate();
+    const { voucherTaskCompletionDispatch } = useVoucherTaskCompletionContext();
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const getVoucherTaskCompletions = async () => {
+            await axiosPrivate
+                .get('/admin/completion')
+                .then((data) => {
+                    console.log(data);
+                    voucherTaskCompletionDispatch({ type: 'GET_TASK_COMPLETIONS', payload: data.data });
+                })
+                .catch((error) => {
+                    console.log(error);
+                    const message = error.response?.data
+                        ? `, ${error.response.data.error}`
+                        : '';
+                    setError(error.message + message);
+                });
+        };
+
+        getVoucherTaskCompletions();
+    }, [voucherTaskCompletionDispatch, axiosPrivate]);
+
+    return { error };
+};
+
+export default useGetVoucherTaskCompletion;
