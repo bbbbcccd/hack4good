116c4789f18686056564d69f249721d82abb42b4
Zac <zacheryngky@gmail.com>
2025-01-17 22:52:13 +0800

    Create context and hook to get task completions

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,28 +1,31 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import { AuthContextProvider } from './context/auth/AuthContext.jsx';
-import { MinimartContextProvider } from './context/MinimartContext.jsx';
-import { VoucherTaskContextProvider } from './context/VoucherTaskContext.jsx';
-import { TransactionContextProvider } from './context/users/TransactionContext.jsx';
-import { UsersContextProvider } from './context/admin/UsersContext.jsx';
-import { AdminsContextProvider } from './context/admin/AdminsContext.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { AuthContextProvider } from "./context/auth/AuthContext.jsx";
+import { MinimartContextProvider } from "./context/MinimartContext.jsx";
+import { VoucherTaskContextProvider } from "./context/VoucherTaskContext.jsx";
+import { VoucherTaskCompletionContextProvider } from "./context/VoucherTaskContext.jsx";
+import { TransactionContextProvider } from "./context/users/TransactionContext.jsx";
+import { UsersContextProvider } from "./context/admin/UsersContext.jsx";
+import { AdminsContextProvider } from "./context/admin/AdminsContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthContextProvider>
       <TransactionContextProvider>
         <MinimartContextProvider>
-          <VoucherTaskContextProvider>
-            <UsersContextProvider>
-              <AdminsContextProvider>
-                <App />
-              </AdminsContextProvider>
-            </UsersContextProvider>
-          </VoucherTaskContextProvider>
+          <VoucherTaskCompletionContextProvider>
+            <VoucherTaskContextProvider>
+              <UsersContextProvider>
+                <AdminsContextProvider>
+                  <App />
+                </AdminsContextProvider>
+              </UsersContextProvider>
+            </VoucherTaskContextProvider>
+          </VoucherTaskCompletionContextProvider>
         </MinimartContextProvider>
       </TransactionContextProvider>
     </AuthContextProvider>
-  </StrictMode>,
+  </StrictMode>
 );
